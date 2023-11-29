7b1530ff457d1107ed0fc39f51ae3fe5d41c9901
Eldor <eldorfozilov@gmail.com>
2023-11-29 20:55:26 +0900

    upload report

--- 2023 Fall Semester/Computer Vision/assignments/assign1/Prob4.py
+++ 2023 Fall Semester/Computer Vision/assignments/assign1/Prob4.py
@@ -4,16 +4,31 @@ from Prob2 import CNN_Model
 from torchvision import transforms
 
    
-def test_model(model, image_path, transform=None):
+def test_model(image_path):
+
+
+    # load model
+    model = CNN_Model()
+    model.load_state_dict(torch.load("checkpoints/Prob3.pth"))
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
    
     model.eval()
+
+    # preprocess image
+    transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225]) # ImageNet mean and std (we use this because we use a pretrained model)
+    ])
 
     with torch.no_grad():
         
         image = cv2.imread(image_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        if transform is not None:
-            image = transform(image)
+        image = transform(image)
         
         image = image.unsqueeze(0).to(device) # add batch dimension and move to GPU if available
 
@@ -26,25 +41,14 @@ def test_model(model, image_path, transform=None):
 
     return predicted_hour, predicted_minute.item()
 
-# load model
 
-model = CNN_Model()
-model.load_state_dict(torch.load("checkpoints/Prob3.pth"))
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
 
-transform = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225]) # ImageNet mean and std (we use this because we use a pretrained model)
-])
+if __name__ == "__main__":
 
-image_path = "dataset/clock_images/1.jpg" # example single image path
+    image_path = "dataset/clock_images/1.jpg" # example single image path
 
-predicted_hour, predicted_minute = test_model(model, image_path, transform)
+    predicted_hour, predicted_minute = test_model(image_path)
 
-print(f"Predicted Hour: {predicted_hour}")
-print(f"Predicted Minute: {predicted_minute}")
+    print(f"Predicted Hour: {predicted_hour}")
+    print(f"Predicted Minute: {predicted_minute}")
 
