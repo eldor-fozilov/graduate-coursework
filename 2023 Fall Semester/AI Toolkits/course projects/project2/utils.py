a6d47eb38fab2c2e0f1a5b39aa904582605789fc
Eldor <eldorfozilov@gmail.com>
2023-12-14 14:46:27 +0900

    initial commit

---
+++ 2023 Fall Semester/AI Toolkits/course projects/project2/utils.py
@@ -0,0 +1,27 @@
+import random
+import torch
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def score(accu):
+    return 100*sigmoid(0.1*(accu-50)) + 100*(1-sigmoid(5))
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def load_checkpoint(filepath, device):
+    checkpoint = torch.load(filepath)
+    model = checkpoint['model']
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model = model.to(device)
+
+    return model
+
+def reset(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
