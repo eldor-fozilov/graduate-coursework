d42dbb52a4ef7c10cda1d037e74f605ca688c201
Eldor <eldorfozilov@gmail.com>
2024-05-30 15:38:02 +0900

    finished the problems 9-10

--- 2024 Spring Semester/3D Vision and Machine Perception/assignments/assign2/python/Q10estimate_params.py
+++ 2024 Spring Semester/3D Vision and Machine Perception/assignments/assign2/python/Q10estimate_params.py
@@ -4,11 +4,15 @@ from Q9estimate_pose import Q9
 
 # Q10
 
+# set the random seed to have the same results
+np.random.seed(0)
+
+
 class Q10(Q9):
     # DO NOT CHANGE HERE!
     def __init__(self):
         super(Q10, self).__init__()
-        
+
         # Test parameter estimation with clean points
         Pc = self.Pc
         Kc, Rc, tc = self.estimate_params(Pc)
@@ -20,7 +24,6 @@ class Q10(Q9):
         self.Kc, self.Rc, self.tc = Kc, Rc, tc
         self.Kn, self.Rn, self.tn = Kn, Rn, tn
 
-
     """
     Q10 Camera Parameter Estimation
         [I] P, camera matrix (3x4 matrix)
@@ -28,27 +31,45 @@ class Q10(Q9):
             R, camera extrinsics rotation (3x3 matrix)
             t, camera extrinsics translation (3x1 matrix)
     """
+
     def estimate_params(self, P):
-        
         """
-        Write your own code here 
-
-
-        
-
-        replace pass by your implementation
+        Write your own code here
         """
-        pass
+        U, S, V_T = np.linalg.svd(P)
+        c = V_T[-1]
+        c = c[:-1] / c[-1]
+        # Remove the last column - 3x4 -> 3x3
+        M = P[:, :-1]
+
+        QR_decomp = la.qr(np.linalg.inv(M))
+        Q = QR_decomp.Q
+        R = QR_decomp.R
+
+        K = np.linalg.inv(R)  # 3x3
+        R = np.linalg.inv(Q)  # 3x3
+
+        # Adjust signs of K and R to ensure positive diagonal of K
+        D = np.diag(np.sign(np.diag(K)))
+        K = K @ D
+        # Normalize the matrix so that the last element to 1
+        K = K / K[-1, -1]
+        R = D @ R
+        t = -np.matmul(R, c)
+
+        return K, R, t
 
 
 if __name__ == "__main__":
 
     Q10 = Q10()
 
-    print('Intrinsic Error with clean 2D points:', la.norm((Q10.Kc/Q10.Kc[-1,-1])-(Q10.K/Q10.K[-1,-1])))
+    print('Intrinsic Error with clean 2D points:', la.norm(
+        (Q10.Kc/Q10.Kc[-1, -1])-(Q10.K/Q10.K[-1, -1])))
     print('Rotation Error with clean 2D points:', la.norm(Q10.R-Q10.Rc))
     print('Translation Error with clean 2D points:', la.norm(Q10.t-Q10.tc))
-    
-    print('Intrinsic Error with noisy 2D points:', la.norm((Q10.Kn/Q10.Kn[-1,-1])-(Q10.K/Q10.K[-1,-1])))
+
+    print('Intrinsic Error with noisy 2D points:', la.norm(
+        (Q10.Kn/Q10.Kn[-1, -1])-(Q10.K/Q10.K[-1, -1])))
     print('Rotation Error with noisy 2D points:', la.norm(Q10.R-Q10.Rn))
     print('Translation Error with noisy 2D points:', la.norm(Q10.t-Q10.tn))
--- 2024 Spring Semester/3D Vision and Machine Perception/assignments/assign2/python/Q9estimate_pose.py
+++ 2024 Spring Semester/3D Vision and Machine Perception/assignments/assign2/python/Q9estimate_pose.py
@@ -3,59 +3,71 @@ import numpy.linalg as la
 
 # Q9
 
+# set the random seed to have the same results
+np.random.seed(0)
+
+
 class Q9():
     # DO NOT CHANGE HERE!
     def __init__(self):
 
         # Generate random camera matrix
-        K = np.array([[1,0,100], [0,1,100], [0,0,1]])
-        R, _,_ = la.svd(np.random.randn(3,3))
-        if la.det(R) < 0: R = -R
-        t = np.vstack((np.random.randn(2,1), 1))
+        K = np.array([[1, 0, 100], [0, 1, 100], [0, 0, 1]])
+        R, _, _ = la.svd(np.random.randn(3, 3))
+        if la.det(R) < 0:
+            R = -R
+        t = np.vstack((np.random.randn(2, 1), 1))
 
         P = K @ np.hstack((R, t))
 
         # Generate random 2D and 3D points
         N = 100
-        X = np.random.randn(N,3)
-        x = P @ np.hstack((X, np.ones((N,1)))).T
-        x = x[:2,:].T / np.vstack((x[2,:], x[2,:])).T
+        X = np.random.randn(N, 3)
+        x = P @ np.hstack((X, np.ones((N, 1)))).T
+        x = x[:2, :].T / np.vstack((x[2, :], x[2, :])).T
 
         # Test pose estimation with clean points
         Pc = self.estimate_pose(x, X)
-        xp = Pc @ np.hstack((X, np.ones((N,1)))).T
-        xp = xp[:2,:].T / np.vstack((xp[2,:], xp[2,:])).T
+        xp = Pc @ np.hstack((X, np.ones((N, 1)))).T
+        xp = xp[:2, :].T / np.vstack((xp[2, :], xp[2, :])).T
 
         # Test pose estimation with noisy points
         xn = x + np.random.rand(x.shape[0], x.shape[1])
         Pn = self.estimate_pose(xn, X)
 
-        xpn = Pn @ np.hstack((X, np.ones((N,1)))).T
-        xpn = xpn[:2,:].T / np.vstack((xpn[2,:], xpn[2,:])).T
+        xpn = Pn @ np.hstack((X, np.ones((N, 1)))).T
+        xpn = xpn[:2, :].T / np.vstack((xpn[2, :], xpn[2, :])).T
 
         self.P, self.K, self.R, self.t = P, K, R, t
         self.x, self.xp, self.xn, self.xpn = x, xp, xn, xpn
         self.Pc = Pc
         self.Pn = Pn
 
-    
     """
     Q9 Camera Matrix Estimation
         [I] x, 2D points (Nx2 matrix)
             X, 3D points (Nx3 matrix)
         [O] P, camera matrix (3x4 matrix)
     """
+
     def estimate_pose(self, x, X):
-        
         """
         Write your own code here 
-
-
-        
-
-        replace pass by your implementation
         """
-        pass
+        A = np.zeros((2*len(x), 12))  # 2N x 12 matrix
+        for i in range(len(x)):
+
+            x_, y_, z_ = X[i, 0], X[i, 1], X[i, 2]
+            u, v = x[i, 0], x[i, 1]
+
+            A[2*i] = np.array([x_, y_, z_, 1, 0, 0, 0,
+                              0, -u*x_, -u*y_, -u*z_, -u])
+            A[2*i+1] = np.array([0, 0, 0, 0, x_, y_, z_,
+                                1, -v*x_, -v*y_, -v*z_, -v])
+
+        U, S, V_T = np.linalg.svd(A)
+        P = V_T[-1].reshape((3, 4))
+        return P
 
 
 if __name__ == "__main__":
@@ -63,7 +75,9 @@ if __name__ == "__main__":
     Q9 = Q9()
 
     print('Reprojection Error with clean 2D points:', la.norm(Q9.x-Q9.xp))
-    print('Pose Error with clean 2D points:', la.norm((Q9.Pc/Q9.Pc[-1,-1])-(Q9.P/Q9.P[-1,-1])))
+    print('Pose Error with clean 2D points:', la.norm(
+        (Q9.Pc/Q9.Pc[-1, -1])-(Q9.P/Q9.P[-1, -1])))
 
     print('Reprojection Error with noisy 2D points:', la.norm(Q9.xn-Q9.xpn))
-    print('Pose Error with noisy 2D points:', la.norm((Q9.Pn/Q9.Pn[-1,-1])-(Q9.P/Q9.P[-1,-1])))
+    print('Pose Error with noisy 2D points:', la.norm(
+        (Q9.Pn/Q9.Pn[-1, -1])-(Q9.P/Q9.P[-1, -1])))
